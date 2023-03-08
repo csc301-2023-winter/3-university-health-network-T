d5b90380a087f44ce8d5ba044c250a4282d1608a
tseaten_1 <1460686504@qq.com>
2023-03-08 17:01:07 -0500

    some error fix on exercise.js

--- Deliverable-2/app/server/routes/exercise.js
+++ Deliverable-2/app/server/routes/exercise.js
@@ -1,38 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const pg = require('pg');
-const jwt = require('jsonwebtoken');
 const { pool } = require('../dbConfig');
 const ver_tools = require('../tools/verifiers');
-const Characters = ['']
 
 
-router.get('/exercise', async (req, res) => {
-    const pid = ver_tools.login_ver(req.token)
-    
+router.get('/exercise', (req, res) => {
+    const pid = ver_tools.login_ver(req.token);
+  
     if (pid < 0) {
         res.sendStatus(403);
     }
-    const exercise = req.params.exercise
-    const character = req.params.character
+
+    const exercise = req.params.exercise;
+    const character = req.params.character;
     const dow = new Date().getDay();
-    var resdata = {exercise: exercise, character: character}
-    try {
-        const exerciseData = await db.one('SELECT Number_Sets, Number_Repetitions FROM PrescribedExercise WHERE PatinetID = $1 AND Exercise = $2 AND Day_Of_Week = $3', [pid, exercise, dow])
-        resdata[no_sets] = exerciseData.Number_Sets;
-        resdata[no_repetitions] = exerciseData.Number_Repet;
-    } catch (err) {
-        res.status(500).send(error);
-    }
-    try {
-        const avatarData = await db.one('SELECT Format, Path FROM Avatar WHERE Exercise = $1 AND character = $2', [exercise, character]);
-        resdata[format] = avatarData.Format;
-        resdata[path] = avatarData.Path;
-      } catch (error) {
-        res.status(500).send(error);
-    }
-    res.status(200).json({
-        message:"get info successfully", 
-        data: resdata
+    var resdata = { exercise: exercise, character: character };
+
+    pool.connect((err, client, done) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        client.query('SELECT Number_Sets, Number_Repetitions FROM PrescribedExercise WHERE PatinetID = $1 AND Exercise = $2 AND Day_Of_Week = $3', [pid, exercise, dow], (err, result) => {
+            done();
+
+            if (err) {
+                res.status(500).send(err);
+                return;
+            }
+
+            const exerciseData = result.rows[0];
+            resdata.no_sets = exerciseData.Number_Sets;
+            resdata.no_repetitions = exerciseData.Number_Repet;
+
+            client.query('SELECT Format, Path FROM Avatar WHERE Exercise = $1 AND character = $2', [exercise, character], (err, result) => {
+                done();
+
+                if (err) {
+                    res.status(500).send(err);
+                    return;
+                }
+
+                const avatarData = result.rows[0];
+                resdata.format = avatarData.Format;
+                resdata.path = avatarData.Path;
+
+                res.status(200).json({
+                    message: "get info successfully",
+                    data: resdata
+                });
+            });
+        });
     });
 });
