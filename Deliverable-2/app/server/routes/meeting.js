5c920d342547d24d6e14531ece1acf043ba39103
Haarowww <75808020+Haarowww@users.noreply.github.com>
2023-03-08 22:41:14 -0500

    Add some meeting methods.

--- Deliverable-2/app/server/routes/meeting.js
+++ Deliverable-2/app/server/routes/meeting.js
@@ -1,22 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../dbConfig');
+const ver_tools = require('../tools/verifiers');
 
-router.use(express.static('public'));
-
-router.get("/:userId", (req, res) => {
-    res.render("meetings", { roomId: req.params.userId });
+router.get('/get-all-meetings', (req, res) => {
+  const pid = ver_tools.login_ver(req.token);
+  if (pid < 0) {
+    return res.status(403).send({ message: 'Invalid credentials' });
+  }
+  pool.query('SELECT Date, Time, MeetingID, MeetingPasscode FROM Meeting WHERE PatientID = $1', [pid])
+  .then((meetingsResult) => {
+      const meetingList = meetingsResult.rows.map((row) => ({
+        date: row.date,
+        time: row.time,
+        meetingId: row.meetingId,
+        meetingPasscode: row.meetingPasscode
+      }));
+      console.log(meetingsList);
+      return res.status(200).json({ message: 'Retrive All Meeting Info Successfully', meeting: meetingList });
+  }).catch((err) => {
+    console.log(err);
+    return res.status(500).send({ message: 'Occur errors when getting relevant data' });
+  });
 });
 
-router.setupSocket = (io) => {
-    io.on("connection", (socket) => {
-        socket.on("join-room", (roomId, userId) => {
-          socket.join(roomId);
-          socket.to(roomId).emit("user-connected", userId);
-          socket.on("disconnect", () => {
-            socket.to(roomId).emit('user-disconnected', userId);
-          });
-        });
-    });
-};
-
+router.get('/get-upcoming-meetings', (req, res) => {
+  const pid = ver_tools.login_ver(req.token);
+  if (pid < 0){
+    return res.status(403).send({ message: 'Invalid credentials' });
+  }
+  pool.query('SELECT Date, Time, MeetingID, MeetingPasscode FROM Meeting WHERE PatientID = $1 ORDER BY Date DESC, Time DESC LIMIT 1', [pid])
+  .then((meetingResult) => {
+    const meeting = meetingResult.rows.map((row) => ({
+      date: row.date,
+      time: row.time,
+      meetingId: row.meetingId,
+      meetingPasscode: row.meetingPasscode
+    }));
+    console.log(meeting);
+    return res.status(200).json({ message: 'Retrive Meeting Info Successfully', meeting: meeting });
+  }).catch((err) => {
+    console.log(err);
+    return res.status(500).send({ message: 'Occur errors when getting relevant data' });
+  });
+});
 module.exports = router;
