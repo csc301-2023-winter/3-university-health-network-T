a34c771c8502e6ec6648372b7eda66210504f3bd
tseaten_1 <1460686504@qq.com>
2023-03-08 19:57:19 -0500

    add more exercise funcs

--- Deliverable-2/app/server/routes/exercise.js
+++ Deliverable-2/app/server/routes/exercise.js
@@ -3,8 +3,76 @@ const router = express.Router();
 const { pool } = require('../dbConfig');
 const ver_tools = require('../tools/verifiers');
 
+router.get('/get-all-exes', (req, res) => {
+    const pid = ver_tools.login_ver(req.token);
+  
+    if (pid < 0) {
+        res.sendStatus(403);
+        return;
+    }
+    pool.query('SELECT Exercise, Character, Number_Sets, Number_Repetitions, Day_Of_Week FROM PrescribedExercise WHERE PatientID = $1', [pid], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+    
+        const data = {};
+        result.rows.forEach(row => {
+            const key = `(${row.Exercise},${row.Day_Of_Week})`;
+            if (!data[key]) {
+                data[key] = {
+                    Characters: [row.Character],
+                    Number_Sets: row.Number_Sets,
+                    Number_Repetitions: row.Number_Repetitions
+            };
+            } else {
+                data[key].Characters.push(row.Character);
+            }
+        });
+    
+        res.status(200).json({
+            message: 'Retrieved exercises successfully',
+            data: data
+        });
+      });
+});
 
-router.get('/exercise', (req, res) => {
+router.get('/getexes-by-dow', (req, res) => {
+    const pid = ver_tools.login_ver(req.token);
+  
+    if (pid < 0) {
+        res.sendStatus(403);
+        return;
+    }
+
+    const dow = req.body.Day_Of_Week;
+    pool.query('SELECT Exercise, Character, Number_Sets, Number_Repetitions FROM PrescribedExercise WHERE PatientID = $1 AND Day_Of_Week = $2', [pid, dow], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+    
+        const data = {};
+        result.rows.forEach(row => {
+            if (!data[row.Exercise]) {
+                data[row.Exercise] = {
+                Characters: [row.Character],
+                Number_Sets: row.Number_Sets,
+                Number_Repetitions: row.Number_Repetitions
+                };
+            } else {
+                data[row.Exercise].Characters.push(row.Character);
+            }
+        });
+    
+        res.status(200).json({
+            message: 'Retrieved exercises successfully',
+            data: {data}
+        });
+    });
+})
+
+router.get('/avatar-for-exes', (req, res) => {
     const pid = ver_tools.login_ver(req.token);
   
     if (pid < 0) {
