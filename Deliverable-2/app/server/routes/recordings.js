27ccf62cb4b0957a35d30d3917a6424454a0391c
tseaten_1 <1460686504@qq.com>
2023-03-09 00:39:27 -0500

    after cloud setting up, finish the todo line in recording.js

--- Deliverable-2/app/server/routes/recordings.js
+++ Deliverable-2/app/server/routes/recordings.js
@@ -4,8 +4,7 @@ const { pool } = require('../dbConfig');
 const multer = require('multer');
 const azure = require('azure-storage');
 const ver_tools = require('../tools/verifiers');
-
-const blobService = azure.createBlobService();
+const blobService = azure.createBlobService(// TODO: after setup server part, fill here); 
 const videoContainerName = 'videos';
 const jointContainerName = 'joints';
 
