e8482c02a2a548543c14b35b8d61f0e221142821
Haarowww <75808020+Haarowww@users.noreply.github.com>
2023-03-09 19:40:40 -0500

    update table and meeting.js

--- Deliverable-2/app/server/routes/pop_up.js
+++ Deliverable-2/app/server/routes/pop_up.js
@@ -3,57 +3,8 @@ const router = express.Router();
 const pg = require('pg');
 const { pool } = require('../dbConfig');
 
-// below is the connection to the database. Currently we set the connection to local database.
-// const pool = new pg.Pool({
-//     user: 'lewis',
-//     host: 'localhost',
-//     database: 'lewis',
-//     password: '',
-//     port: 5432,
-//     // ssl: true // when we connect to cloud, activate the ssl connection.
-// });
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 // below post method is for testing purposes only. It will add some information to the database.
-router.post('/popup', (req, res) => {
-    const query = `
-    CREATE TABLE IF NOT EXISTS PrescribedExercise(
-        userId INTEGER,
-        exercise text,
-        character text,
-        number_sets INTEGER,
-        number_repetitions INTEGER,
-        day_of_week text
-    )`;
-    pool.query(query).then(
-        () => {
-            console.log('create this query successfully');
-        }).catch((err) => console.log(err));
-    const { userId, exercise, character, number_sets, number_repetitions, day_of_week, date, time, meetingId, meetingPasscode } = req.body;
-    pool.query('INSERT INTO PrescribedExercise (userId, exercise, character, number_sets, number_repetitions, day_of_week) VALUES ($1, $2, $3, $4) RETURNING id', [userId, exercise, character, number_sets, number_repetitions, day_of_week])
-    .then((result) => {
-        console.log(result);
-        console.log('create this query successfully');
-    }).catch((err) => console.log(err));
-    const meetingquery = `
-    CREATE TABLE IF NOT EXISTS Meeting(
-        userId INT,
-        date DATE,
-        time TIME,
-        meetingId INT,
-        meetingPasscode VARCHAR(50)
-    )`;
-    pool.query(meetingquery).then(
-        () => {
-            console.log('create meeting table successfully');
-        }
-    ).catch((err) => console.log(err));
-    pool.query('INSERT INTO Meeting (userId, date, time, meetingId, meetingPasscode) VALUES ($1, $2, $3, $4, $5) RETURNING id', [userId, date, time, meetingId, meetingPasscode])
-    .then((result) => {
-        console.log(result);
-        console.log('create meeting query successfully');
-    }).catch((err) => console.log(err));
-    return res.send('Add patient info successfully');
-});
 
 router.get('/popup', async (req, res) => {
     try {
