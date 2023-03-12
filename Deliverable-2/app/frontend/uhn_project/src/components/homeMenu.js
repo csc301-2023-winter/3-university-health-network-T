205cc0d5c9f93ab619bac1dab1f90044f513424c
nzhou27 <zhouhanzhi0818@gmail.com>
2023-03-12 17:10:23 -0400

    added fetch for home pop up page and the component is ready to be used as an actual pop up once recording page is done

--- Deliverable-2/app/frontend/uhn_project/src/components/homeMenu.js
+++ Deliverable-2/app/frontend/uhn_project/src/components/homeMenu.js
@@ -2,10 +2,12 @@ import Menu from "../Component/Menu"
 import HomePopUP from "./homePopUP"
 import "./homePopUp.css";
 function HomeMove(){
+
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNjc4NjUyNzE1LCJleHAiOjE2Nzg3MzkxMTV9.IEwM7IN28HfX9xPfVgd9Rq88bEUYGerUJm47DlqUY-4";
     return(
         <div id="screen">
             <Menu/>
-            <HomePopUP/>
+            <HomePopUP token={token}/>
         </div>
 
     )
--- Deliverable-2/app/frontend/uhn_project/src/components/homePopUP.js
+++ Deliverable-2/app/frontend/uhn_project/src/components/homePopUP.js
@@ -1,54 +1,67 @@
-import Button from "react-bootstrap/Button";
-import "react-icons/bs";
-import "./homePopUp.css";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DateDisplay from './formatDate';
+import {Button} from "react-bootstrap";
 
-const HomePopUP = () => {
+function HomePopUP({ token }) {
+  const [exerciseData, setExerciseData] = useState(null);
+  const [meetingData, setMeetingData] = useState(null);
 
-  let navigate = useNavigate(); 
-  const continueRecording = () =>{ 
-    let path = `/video`; 
-    navigate(path);
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
+
+  const today = new Date();
+  const formattedDate = formatDate(today);
   
 
+  useEffect(() => {
+    axios.get('http://localhost:5000/popup', {
+      headers: { 'Authorization': `Bearer ${token}` }
+    })
+      .then(response => {
+        const data = response.data.data;
+        setExerciseData(data.exercises);
+        setMeetingData(data.upcoming_m);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [token]);
+
+  if (!exerciseData || !meetingData) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      {/* <div className=" menu-buttons">
-    
-        <div className="icons">
-       
-          <BiMenu size={38} />
-        </div> 
-         <div className="icons">
-          <AiOutlineQuestion size={38} />
-        </div> 
-      </div> */}
-
-      <div id="pop-up-box">
-        <div id="list-exercise">
-          <p>
-            Feb 6, Monday,
-            <br /> you prescribed exercise today:
-          </p>
-          <br />
-          <ol>1. Leg extension, 2 sets of 8 rep</ol>
-          <ol>2. Lift the arms, 2 sets of 10 reps</ol>
-          <ol>3. ....</ol>
-
-          <Button className="buttons" onClick={continueRecording}>Continue</Button>
-          <br />
-          <Button className="buttons" onClick={continueRecording}>Restart</Button>
-        </div>
-
-        <div id="list-meetings">
-          <p>Upcoming meeting:</p>
-          <ol>Feb 21, 11:30 - 12:30 </ol>
-          <Button className="buttons">Join</Button>
-        </div>
+    <div id="out-box">
+      <DateDisplay date={formattedDate} />
+      <p>Your prescribed exercise for today:</p><br/>
+      <div id="list-exercise">
+      <h5>Exercise Info</h5>
+      <ol>
+        {exerciseData.map(exercise => (
+          <li key={exercise.exercise}>
+            {exercise.exercise}: {exercise.number_sets} sets of {exercise.number_repetitions} repetitions
+          </li>
+        ))}
+      </ol>
+      <Button className='buttons'>Continue</Button>
+      <Button className='buttons'>Restart</Button>
       </div>
-    </>
+      <div id="list-meetings"><br/>
+      <h5 >Upcoming Meeting</h5>
+      {meetingData.date.slice(0, 10)},  {meetingData.starttime.slice(0,5)} - {meetingData.endtime.slice(0,5)}</div><br/>
+      <Button className='buttons'>Join</Button>
+
+    </div>
   );
-};
+}
+
+
+
 
 export default HomePopUP;
