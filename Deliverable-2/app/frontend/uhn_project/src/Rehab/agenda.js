6ea23bf9aa335f8d9daa5149eb6b4fef45b08534
nzhou27 <zhouhanzhi0818@gmail.com>
2023-03-15 22:16:23 -0400

    finished agenda function

--- Deliverable-2/app/frontend/uhn_project/src/Rehab/agenda.js
+++ Deliverable-2/app/frontend/uhn_project/src/Rehab/agenda.js
@@ -1,61 +1,62 @@
 import { useState, useEffect } from 'react';
+import "./agenda.css";
 
-const Agenda = (token) =>{
-    // const [calendarData, setCalendarData] = useState(null);
+const Agenda = () =>{
+  const [data, setData] = useState(null);
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNjc4OTI5NzA2LCJleHAiOjE2NzkwMTYxMDZ9.Z1X8zdWOGYN5wJb0c3AMh2Wg-KTOSk0TRmNQDJAeQPs";
 
-    // useEffect(() => {
-    //   const fetchCalendar = async () => {
-    //     const response = await fetch('calendar/days', {
-    //       method: 'GET',
-    //       headers: {
-    //         'Authorization': `Bearer ${token}`
-    //       }
-    //     });
-    //     const data = await response.json();
-    //     setCalendarData(data);
-    //   };
-  
-    //   fetchCalendar();
-    // }, [token]);
-  
-    // if (!calendarData) {
-    //   return <div>Loading...</div>;
-    // }
-  
-    // const { message, data } = calendarData;
-  
-    // if (message !== 'success') {
-    //   return <div>Error: {message}</div>;
-    // }
-  
-    // const { dates } = data;
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`http://localhost:5000/calendar/day`, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+      const responseData = await response.json();
+      setData(responseData.data);
+    }
+    fetchData();
+  }, [token]);
 
-  
-    return (
-    //   <div>
-    //     {dates.map(date => (
-    //       <div key={date.date}>
-    //         <h2>{date.date}</h2>
-    //         {date.exes.map(exercise => (
-    //           <div key={exercise.exercise}>
-    //             <h3>{exercise.exercise}</h3>
-    //             <p>Sets: {exercise.no_sets}</p>
-    //             <p>Reps: {exercise.no_reps}</p>
-    //           </div>
-    //         ))}
-    //         {date.meeting.map(meeting => (
-    //           <div key={meeting.mid}>
-    //             <h3>Meeting</h3>
-    //             <p>Start Time: {meeting.starttime}</p>
-    //             <p>End Time: {meeting.endtime}</p>
-    //           </div>
-    //         ))}
-    //       </div>
-    //     ))}
-    //   </div>
-    <h3>agenda to be implemented</h3>
-    );
+  if (!data) {
+    return <div>Loading...</div>;
   }
+
+  return (
+    <div className="display-container">
+    {Object.keys(data).map((dateKey) => (
+      <div key={dateKey} className="each-date">
+        <h2 className="date-header">{dateKey}</h2>
+        {data[dateKey].exercises?.length > 0 && (
+          <div className="exercises-list">
+            <h5>Exercises</h5>
+            <ul>
+              {data[dateKey].exercises.map((exercise, index) => (
+                <li key={index} className="exercise-item">
+                  <p className="exercise-name">{exercise.exercise}</p>
+                  <p className="exercise-sets-repetitions">Sets: {exercise.number_sets}, Repetitions: {exercise.number_repetitions}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {data[dateKey].meetings?.length > 0 && (
+          <div className="meetings-list">
+            <h5>Meetings</h5>
+            <ul>
+              {data[dateKey].meetings.map((meeting, index) => (
+                <li key={index} className="meeting-item">
+                  <p className="meeting-id">Meeting ID: {meeting.meetingid}</p>
+                  <p className="meeting-time">Start Time: {meeting.starttime}, End Time: {meeting.endtime}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    ))}
+  </div>
+  );
+}
+
 
 
 export default Agenda;
