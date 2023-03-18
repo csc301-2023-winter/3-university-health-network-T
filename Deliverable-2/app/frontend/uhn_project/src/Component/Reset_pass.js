f7d2c978d9d15b367711beb7b887300baf039c8d
Richard202019Wang <82163830+Richard202019Wang@users.noreply.github.com>
2023-03-18 00:07:54 -0400

    add reset functionality

--- Deliverable-2/app/frontend/uhn_project/src/Component/Reset_pass.js
+++ Deliverable-2/app/frontend/uhn_project/src/Component/Reset_pass.js
@@ -9,6 +9,8 @@ function ForgotPassword() {
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState('');
   const navigate = useNavigate();
+  const [password, setPassword] = useState('');
+
 
   const handleHelpClick = () => {
     navigate('/help');
@@ -17,19 +19,29 @@ function ForgotPassword() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch(`/api/users?email=${encodeURIComponent(email)}`);
-      if (response.ok) {
+      const response = await fetch('http://localhost:5000/account/reset', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+  
+      const data = await response.json();
+  
+      if (response.status === 200) {
         setSubmitted(true);
         localStorage.removeItem('token');
         localStorage.removeItem('userId');
-        navigate('/login'); 
+        navigate('/');
       } else {
-        setError('Email address not found.');
+        setError(data.message);
       }
     } catch (err) {
       setError('Something went wrong.');
     }
   };
+  
 
   if (submitted) {
     return <div>Check your email to reset your password.</div>;
@@ -37,22 +49,24 @@ function ForgotPassword() {
 
   return (
     <div className='forgot-password-container'>
-        {/* <div style={{ width: '500px', height: '500px' ,marginRight: '20px'}}>
-            <img src={uhn_logo} alt="Login" style={{ width: '100%', height: '100%' }}/>
-        </div> */}
-        <div style={{ position: 'absolute', top: '0', left: '0' }}>
-            <AiOutlineQuestion className="menu-button2" size={38} onClick = {handleHelpClick}/>
-        </div>
-        <form className="forgot-password-form" onSubmit={handleSubmit}>
+      <div style={{ position: 'absolute', top: '0', left: '0' }}>
+        <AiOutlineQuestion className="menu-button2" size={38} onClick={handleHelpClick} />
+      </div>
+      <form className="forgot-password-form" onSubmit={handleSubmit}>
         <label className='forgot-password-label'>
-            Enter your email address:
-            <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+          Enter your email address:
+          <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+        </label>
+        <label className='forgot-password-label'>
+          Enter your new password:
+          <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
         </label>
         {error && <div className="error">{error}</div>}
         <button className='forgot-password-button' type="submit">Reset Password</button>
-        </form>
+      </form>
     </div>
   );
+  
 }
 
 export default ForgotPassword;
