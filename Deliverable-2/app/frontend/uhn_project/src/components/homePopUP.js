29a07f9bf28bbad89c7368fe7627f8f2cc395157
Richard202019Wang <82163830+Richard202019Wang@users.noreply.github.com>
2023-03-28 22:00:40 -0400

    fix bug

--- Deliverable-2/app/frontend/uhn_project/src/components/homePopUP.js
+++ Deliverable-2/app/frontend/uhn_project/src/components/homePopUP.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DateDisplay from './formatDate';
-import {Button} from "react-bootstrap";
+import { Button } from 'react-bootstrap';
 
 function HomePopUP() {
   const [exerciseData, setExerciseData] = useState(null);
@@ -16,60 +16,74 @@ function HomePopUP() {
 
   const today = new Date();
   const formattedDate = formatDate(today);
-  const token = localStorage.getItem("token");
-  
+  const token = localStorage.getItem('token');
 
   useEffect(() => {
-    axios.get('http://localhost:4000/popup', {
-      headers: { 'Authorization': `Bearer ${token}` }
-    })
-      .then(response => {
+    axios
+      .get('http://localhost:5000/popup', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
         const data = response.data.data;
         setExerciseData(data.exercises);
         setMeetingData(data.upcoming_m);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }, [token]);
 
-
   const renderExercise = () => {
-    return (<ol>
-      {exerciseData?.map(exercise => (
-        <li key={exercise.exercise}>
-          {exercise.exercise}: {exercise.number_sets} sets of {exercise.number_repetitions} repetitions
-        </li>
-      ))}
-    </ol>);
-  }
+    return (
+      <ol>
+        {exerciseData?.map((exercise) => (
+          <li key={exercise.exercise}>
+            {exercise.exercise}: {exercise.number_sets} sets of {exercise.number_repetitions} repetitions
+          </li>
+        ))}
+      </ol>
+    );
+  };
 
-  
+  const renderMeeting = () => {
+    if (meetingData) {
+      return (
+        <>
+          {meetingData.date.slice(0, 10)} {meetingData.starttime.slice(0, 5)} -{' '}
+          {meetingData.endtime.slice(0, 5)}
+        </>
+      );
+    } else {
+      return ' -- There is no upcoming meeting';
+    }
+  };
 
   return (
     <div id="out-box">
       <DateDisplay date={formattedDate} />
-      <p>Your prescribed exercise for today:</p><br/>
+      <p>Your prescribed exercise for today:</p>
+      <br />
       <div id="list-exercise">
-      <h5>Exercise Info</h5>
-      {exerciseData? renderExercise(): "There is no prescribed exercise"}
-      
-      <a href="/video"><Button className='buttons'>Continue</Button></a>
-      <a href="/video"><Button className='buttons'>Restart</Button></a>
+        <h5>Exercise Info</h5>
+        {exerciseData ? renderExercise() : 'There is no prescribed exercise'}
+
+        <a href="/video">
+          <Button className="buttons">Continue</Button>
+        </a>
+        <a href="/video">
+          <Button className="buttons">Restart</Button>
+        </a>
       </div>
-      <div id="list-meetings"><br/>
-      <h5 >Upcoming Meeting</h5>
-      {meetingData ? (meetingData.date.slice(0, 10),  meetingData.starttime.slice(0,5) - meetingData.endtime.slice(0,5)) : " -- There is no upcoming meeting"}
-
-     
-      </div><br/> 
-      <Button className='buttons'>Join</Button>
-
+      <div id="list-meetings">
+        <br />
+        <h5>Upcoming Meeting</h5>
+        {renderMeeting()}
+      </div>
+      <br />
+      <Button className="buttons">Join</Button>
     </div>
   );
 }
 
-
-
-
 export default HomePopUP;
+
