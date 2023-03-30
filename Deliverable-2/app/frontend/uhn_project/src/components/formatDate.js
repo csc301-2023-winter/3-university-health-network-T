11e9fec62dfdc1317544c8adcf1917921c92c81f
Vanessa-S <67811325+vanessa-sissi@users.noreply.github.com>
2023-03-29 20:27:13 -0400

    Update formatDate.js
    Add explanation of the responsiveness of the code

--- Deliverable-2/app/frontend/uhn_project/src/components/formatDate.js
+++ Deliverable-2/app/frontend/uhn_project/src/components/formatDate.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//provides a reusable and flexible way to display formatted dates in React applications.
+//By passing in a date string as a prop, the component can display the formatted date in a consistent and user-friendly way.
+
 function formatDate(dateString) {
   const dateObj = new Date(dateString);
   const month = dateObj.toLocaleString('en-us', { month: 'long' });
